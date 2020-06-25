ff074fbd3600fc1a8025b637eac027695c7b6576
craftycram <github@craftycram.net>
2020-06-25 11:46:16 +0200

    added component

---
+++ src/components/interface.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-container>
+  <b-row>
+    <b-col style="background-color: red;">test</b-col>
+    <b-col style="background-color: green;">hello</b-col>
+    <b-col style="background-color: blue;">world</b-col>
+  </b-row>
+</b-container>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
