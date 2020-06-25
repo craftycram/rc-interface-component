d8e1d21f1cf9effc32d50d97303f3f744cec2afa
craftycram <github@craftycram.net>
2020-06-25 12:23:04 +0200

    changed layout

--- src/components/interface.vue
+++ src/components/interface.vue
@@ -1,11 +1,11 @@
 <template>
   <b-container>
   <b-row>
-    <b-col style="background-color: yellow; min-height: 720px;">Video</b-col>
+    <b-col class="grid" style="background-color: yellow; min-height: 720px;">Video</b-col>
   </b-row>
   <b-row>
-    <b-col style="background-color: cyan; min-height: 200px;">controls / queue</b-col>
-    <b-col style="background-color: lightgreen; min-height: 200px;">infos</b-col>
+    <b-col class="grid" style="background-color: cyan; min-height: 200px;">controls / queue</b-col>
+    <b-col class="grid" style="background-color: lightgreen; min-height: 200px;">infos</b-col>
   </b-row>
 </b-container>
 </template>
@@ -17,5 +17,8 @@ export default {
 </script>
 
 <style>
+.grid {
+  margin: 5px;
+}
 
 </style>
