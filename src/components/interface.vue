ec0dcdf7435716b5acb66819525de4232d3d510c
craftycram <github@craftycram.net>
2020-07-23 12:29:44 +0200

    added slots

--- src/components/interface.vue
+++ src/components/interface.vue
@@ -1,11 +1,20 @@
 <template>
   <b-container>
   <b-row>
-    <b-col class="grid" style="background-color: yellow; min-height: 720px;">Video</b-col>
+    <b-col class="grid" style="background-color: yellow; min-height: 720px;">
+      Video
+      <slot name="videocomponent"></slot>
+    </b-col>
   </b-row>
   <b-row>
-    <b-col class="grid" style="background-color: cyan; min-height: 200px;">controls / queue</b-col>
-    <b-col class="grid" style="background-color: lightgreen; min-height: 200px;">infos</b-col>
+    <b-col class="grid" style="background-color: cyan; min-height: 200px;">
+      controls
+      <slot name="controlcomponent"></slot>
+    </b-col>
+    <b-col class="grid" style="background-color: lightgreen; min-height: 200px;">
+      infos / queue
+      <slot name="queuecomponent"></slot>
+      </b-col>
   </b-row>
 </b-container>
 </template>
