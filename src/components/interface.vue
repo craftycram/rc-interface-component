a68b8e6ab50ce815602afd15662998b8abf210b6
craftycram <github@craftycram.net>
2020-07-27 15:37:15 +0200

    removed texts

--- src/components/interface.vue
+++ src/components/interface.vue
@@ -2,17 +2,14 @@
   <b-container>
   <b-row>
     <b-col class="grid" style="background-color: yellow; min-height: 720px;">
-      Video
       <slot name="videocomponent"></slot>
     </b-col>
   </b-row>
   <b-row>
     <b-col class="grid" style="background-color: cyan; min-height: 200px;">
-      controls
       <slot name="controlcomponent"></slot>
     </b-col>
     <b-col class="grid" style="background-color: lightgreen; min-height: 200px;">
-      infos / queue
       <slot name="queuecomponent"></slot>
       </b-col>
   </b-row>
